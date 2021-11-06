488e047ec9c3e7343d045525e862a4c62ed32adf
Josué Rivas <josuerivas2311@gmail.com>
2021-11-06 13:13:55 -0600

    refactor(component): now display data dynamically

---
+++ src/components/anime/ui-actions/AnimeViewAnimes/data/animeViewData.js
@@ -0,0 +1,22 @@
+export const animeViewData = [
+  {
+    id: 1,
+    title: 'Todas',
+    class: 'view-note-active',
+    category: 'ALL',
+  },
+  {
+    id: 2,
+    title: 'Activas',
+    class: 'view-note-active',
+    category: 'PENDING',
+    state: false,
+  },
+  {
+    id: 3,
+    title: 'Completadas',
+    class: 'view-note-active',
+    state: true,
+    category: 'COMPLETED',
+  },
+];
